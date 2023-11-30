93fa36cd6a05d5ad4da4c6fa04c4fc3b8eae293d
11 <satish.b@fluentgrid.com>
2023-11-30 16:27:29 +0530

    ok

--- egov-mdms-service-test/src/main/java/org/egov/MDMSApplication.java
+++ egov-mdms-service-test/src/main/java/org/egov/MDMSApplication.java
@@ -10,5 +10,5 @@ public class MDMSApplication {
     public static void main(String[] args) {
         SpringApplication.run(MDMSApplication.class, args);
     }
-
+//
 }
--- egov-mdms-service-test/target/typescript-generator/egov-mdms-service-test.d.ts
+++ egov-mdms-service-test/target/typescript-generator/egov-mdms-service-test.d.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 2.22.595 on 2023-11-30 15:17:13.
+// Generated using typescript-generator version 2.22.595 on 2023-11-30 16:27:14.
 
 export namespace Digit {
 
